e1a17479bde0ba62db9adb21bef37712eede66d1
Salla Douglas <sallanz77@gmail.com>
2023-07-15 14:42:45 +1200

    Add .php file for contact form

---
+++ form-to-email.php
@@ -0,0 +1,20 @@
+<?php
+    $name = $_POST['name'];
+    $visitor_email = $_POST['email'];
+    $visitor_phone = $_POST['phone'];
+    $message = $_POST['message'];
+?>
+
+<?php
+    $email_from = 'salty@saltyservices.co.nz';
+    $email_subject = 'New form submission';
+    $email_body = 'You have a message from user $name.\n'.
+                    'It says:\n $message'
+?>
+
+<?php
+    $to = 'salty@saltyservices.co.nz';
+    $headers = 'From: $email_from \r\n';
+    $headers .='Reply-To: $visitor_email \r\n';
+    mail($to,$email_subject,$email_body,$headers);
+?>
